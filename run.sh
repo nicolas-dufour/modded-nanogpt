ef4ee4dc06235cf8f89b1fbd6ad55ecd38eb3481
Nicolas Dufour <nicolas.dufourn@gmail.com>
2025-03-31 16:52:59 +0200

    Update run.sh to support multi-node training configuration

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-torchrun --standalone --nproc_per_node=8 train_gpt.py
+torchrun --nnodes=2 --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --nproc_per_node=4 train_gpt.py
